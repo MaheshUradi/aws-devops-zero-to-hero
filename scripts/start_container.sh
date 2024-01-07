6a1b7988a0f0525c2095efd23a4208880041d2c9
MaheshUradi <107531928+MaheshUradi@users.noreply.github.com>
2024-01-07 23:30:00 +0530

    commit the changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull maheshuradi/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 maheshuradi/simple-python-flask-app
